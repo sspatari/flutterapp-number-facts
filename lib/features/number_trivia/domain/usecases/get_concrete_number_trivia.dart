df57d7bece20334edfa109ebf08def48c3ec2f01
sspatari <spatari.stanislav@gmail.com>
2020-05-06 01:14:40 +0300

    feat(number_trivia): implement getRandomNumberTrivia usecase

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,15 +1,24 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutterapp/core/error/failures.dart';
+import 'package:flutterapp/core/usecases/usecase.dart';
 import 'package:flutterapp/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutterapp/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:meta/meta.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements Usecase<NumberTrivia, Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>> call({@required int number}) async {
-    return await repository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({@required this.number}) : super([number]);
 }
