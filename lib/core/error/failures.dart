399af56890af776cdcc8fe1b6ce9d91bfc92d741
sspatari <spatari.stanislav@gmail.com>
2020-06-07 16:28:50 +0300

    chore(equatable): update version to 1.1.1

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,10 +1,14 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable {
-  Failure([List proprieties = const <dynamic>[]]) : super([proprieties]);
-}
+abstract class Failure extends Equatable {}
 
 // General failures
-class ServerFailure extends Failure {}
+class ServerFailure extends Failure {
+  @override
+  List<Object> get props => [];
+}
 
-class CacheFailure extends Failure {}
+class CacheFailure extends Failure {
+  @override
+  List<Object> get props => [];
+}
--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -20,5 +20,8 @@ class GetConcreteNumberTrivia implements Usecase<NumberTrivia, Params> {
 class Params extends Equatable {
   final int number;
 
-  Params({@required this.number}) : super([number]);
+  Params({@required this.number});
+
+  @override
+  List<Object> get props => [number];
 }
